96c578925b9af689efb43f63ffdf87c9d55b0086
Arotăriței Robert, R <r.arotaritei@student.fontys.nl>
2020-09-25 16:51:15 +0200

    Add Sorting Bar to Menu

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import SortBar from '../components/SortBar'
 import ProductsList from '../components/ProductsList';
 
 const MenuPage = () => {
     return(
         <div>
             <NavBar />
+            <SortBar />
             <ProductsList />
         </div>
     )
--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -3,9 +3,9 @@ import React from 'react';
 const NotFoundPage = () => {
     return(
         <div style={{padding: '3em'}}>
-            <h3>Sorry,</h3>
+            <h3>Oh Sorry,</h3>
             <br></br>
-            <h3>This page does not exist</h3>
+            <h3>It seems that this page does not exist</h3>
         </div>
     )
 }
