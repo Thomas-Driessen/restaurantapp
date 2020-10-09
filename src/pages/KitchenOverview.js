eceb9bc7e4562051054b8c46c0c2ca32908c5300
Rayco <raycoh1299@gmail.com>
2020-10-09 15:19:31 +0200

    Kitchenoverview

--- src/pages/KitchenOverview.js
+++ src/pages/KitchenOverview.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import updateKitchenOverview from '../components/updateKitchenOverview'
 import tableId from '../components/TableId'
+import { Typography } from '@material-ui/core';
+import NavBar from '../components/Navigation bars/NavBar';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { spacing } from '@material-ui/system';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 
 class KitchenOverview extends React.Component {
     constructor() {
@@ -59,19 +68,140 @@ class KitchenOverview extends React.Component {
     render(){
         return(
             <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <div>
-                                <div>Receved</div>
-                                <div id="receved-column"></div>
-                            </div>
-                            <div>Progress</div>
-                            <div>Done</div>
-                        </tr>
-                    </thead>
-                    <tbody id="Orders"></tbody>   
-                </table> 
+                <NavBar/>
+                <Grid container spacing={0} style={{padding: 5}}>
+                    <Grid item xs={12} sm={6} lg={4} xl={4} style={{padding: 25}}>
+                        <h2>Todo</h2>
+                        <div class="background-lightgray kitchen-overview-container">
+
+                            {/* START - todo Placeholder item 1 */}
+                            <Box mb={3}>
+                                <Card>
+                                    <CardContent>
+                                        <Box display="flex" justifyContent="space-between">
+                                            <Typography display="inline" variant="h4" component="h3">
+                                            Pizza
+                                            </Typography>
+                                            <Typography ml={5} display="inline" color="textPrimary">
+                                            Table 3
+                                            </Typography>
+                                        </Box>
+                                        <Typography display="inline" color="textPrimary">
+                                        Note:
+                                        </Typography>
+                                        <Typography gutterleft display="inline" color="textSecondary" component="p">
+                                        No gluten
+                                        </Typography>
+                                    </CardContent>
+                                    <Box display="flex" justifyContent="flex-end" p={1}>
+                                        <Box mr={2} display="flex" alignItems="center">
+                                            <Typography color="textSecondary"> 6 minutes ago </Typography>
+                                        </Box>
+                                        <Button variant="contained" color="primary" size="medium">Start</Button>
+                                    </Box>
+                                </Card>
+                            </Box>
+                            {/* END - todo Placeholder item 1 */}
+                            {/* START - todo Placeholder item 2 */}
+                            <Box mb={3}>
+                                <Card>
+                                    <CardContent>
+                                        <Box display="flex" justifyContent="space-between">
+                                            <Typography display="inline" variant="h4" component="h3">
+                                            Root beer
+                                            </Typography>
+                                            <Typography ml={5} display="inline" color="textPrimary">
+                                            Table 3
+                                            </Typography>
+                                        </Box>
+                                        <Typography display="inline" color="textPrimary">
+                                        Note:
+                                        </Typography>
+                                        <Typography gutterleft display="inline" color="textSecondary" component="p">
+                                        
+                                        </Typography>
+                                    </CardContent>
+                                    <Box display="flex" justifyContent="flex-end" p={1}>
+                                        <Box mr={2} display="flex" alignItems="center">
+                                            <Typography color="textSecondary"> 6 minutes ago </Typography>
+                                        </Box>
+                                        <Button variant="contained" color="primary" size="medium">Start</Button>
+                                    </Box>
+                                </Card>
+                            </Box>
+                            {/* END - todo Placeholder item 2 */}
+
+
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={4} xl={4} style={{padding: 25}}>
+                        <h2>progress</h2>
+                        <div class="background-lightgray kitchen-overview-container">
+
+
+                            {/* START - progress Placeholder item 1 */}
+                            <Box mb={3}>
+                                <Card>
+                                    <CardContent>
+                                        <Box display="flex" justifyContent="space-between">
+                                            <Typography display="inline" variant="h4" component="h3">
+                                            Pizza
+                                            </Typography>
+                                            <Typography ml={5} display="inline" color="textPrimary">
+                                            Table 3
+                                            </Typography>
+                                        </Box>
+                                        <Typography display="inline" color="textPrimary">
+                                        Note:
+                                        </Typography>
+                                        <Typography gutterleft display="inline" color="textSecondary" component="p">
+                                        No gluten
+                                        </Typography>
+                                    </CardContent>
+                                    <Box display="flex" justifyContent="flex-end" p={1}>
+                                        <Box mr={2} display="flex" alignItems="center">
+                                            <Typography color="textSecondary"> 6 minutes ago </Typography>
+                                        </Box>
+                                        <Button variant="contained" color="secondary" size="medium">finish</Button>
+                                    </Box>
+                                </Card>
+                            </Box>
+                            {/* END - progress Placeholder item 1 */}
+
+
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} sm={12} lg={4} xl={4} style={{padding: 25}}>
+                        <h2>done</h2>
+                        <div class="background-lightgray kitchen-overview-container">
+
+
+                            {/* START - done Placeholder item 1 */}
+                            <Box mb={3}>
+                                <Card>
+                                    <CardContent>
+                                        <Box display="flex" justifyContent="space-between">
+                                            <Typography display="inline" variant="h4" component="h3">
+                                            Pizza
+                                            </Typography>
+                                            <Typography ml={5} display="inline" color="textPrimary">
+                                            Table 3
+                                            </Typography>
+                                        </Box>
+                                    </CardContent>
+                                    <Box display="flex" justifyContent="flex-end" p={1}>
+                                        <Box mr={2} display="flex" alignItems="center">
+                                            <Typography color="textSecondary"> 6 minutes ago </Typography>
+                                        </Box>
+                                    </Box>
+                                </Card>
+                            </Box>
+                            {/* END - done Placeholder item 1 */}
+
+
+                        </div>
+                    </Grid>
+                </Grid>
             </div>
         )}
 }
