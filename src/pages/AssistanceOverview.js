90d80919479953c87d6e83bf944356987694ee98
StanEngels <stanengels@gmail.com>
2021-01-08 11:40:06 +0100

    Cors anywhere for hub requests (test)

--- src/pages/AssistanceOverview.js
+++ src/pages/AssistanceOverview.js
@@ -48,7 +48,7 @@ class MenuPage extends React.Component {
 
   ConnectToHub() {
     const hubConnection = new HubConnectionBuilder()
-      .withUrl(`https://s3-restaurant-api.herokuapp.com/Order`)
+      .withUrl(`https://cors-anywhere.herokuapp.com/https://s3-restaurant-api.herokuapp.com/Order`)
       .configureLogging(LogLevel.Information)
       .build();
 
--- src/pages/KitchenOverview.js
+++ src/pages/KitchenOverview.js
@@ -31,7 +31,7 @@ class KitchenOverview extends React.Component {
 
     ConnectToHub() {
         const hubConnection = new HubConnectionBuilder()
-            .withUrl(`https://s3-restaurant-api.herokuapp.com/Order`)
+            .withUrl(`https://cors-anywhere.herokuapp.com/https://s3-restaurant-api.herokuapp.com/Order`)
             .configureLogging(LogLevel.Information)
             .build();
 
