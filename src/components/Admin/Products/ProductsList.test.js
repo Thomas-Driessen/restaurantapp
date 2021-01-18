7aa59dd88240ae56b81a1e0252858798475cca63
robertarotaritei <robert_aro@yahoo.com>
2021-01-18 10:32:58 +0100

    Update snapshots

--- src/components/Admin/Products/ProductsList.test.js
+++ src/components/Admin/Products/ProductsList.test.js
@@ -3,36 +3,33 @@ import { render } from '@testing-library/react';
 import ProductsList from './ProductsList';
 import renderer from 'react-test-renderer';
 
-const products = [
-  {
-    id: 1,
-    title: "Coca-Cola",
-    price: 2.5,
-    description: "",
-    category: { categoryName: "Drinks" },
-    ingredients: [{
-      ingredient: [{
-        ingredientTitle: ""
-      }]
-    }]
-  },
-  {
-    id: 2,
-    title: "Pepsi",
-    price: 2,
-    description: "",
-    category: { categoryName: "Drinks" },
-    ingredients: [{
-      ingredient: [{
-        ingredientTitle: ""
-      }]
-    }]
-  },
-];
-
 const categories = [
-  { categoryName: "Drinks" },
-  { categoryName: "Coffee" }
+  {products: [
+    {
+      id: 1,
+      title: "Coca-Cola",
+      price: 2.5,
+      description: "",
+      category: { categoryName: "Drinks" },
+      ingredients: [{
+        ingredient: [{
+          ingredientTitle: ""
+        }]
+      }]
+    },
+    {
+      id: 2,
+      title: "Pepsi",
+      price: 2,
+      description: "",
+      category: { categoryName: "Drinks" },
+      ingredients: [{
+        ingredient: [{
+          ingredientTitle: ""
+        }]
+      }]
+    },
+  ]}
 ]
 
 const ingredients = [
@@ -45,20 +42,20 @@ const ingredients = [
 ]
 
 test('Render Product Coca-Cola', () => {
-  const { getByText } = render(<ProductsList products={products} foodCategories={categories} drinkCategories={categories} ingredients={ingredients} />);
+  const { getByText } = render(<ProductsList products={categories.products} foodCategories={categories} categories={categories} drinkCategories={categories} ingredients={ingredients} />);
   const linkElement = getByText(/Coca-cola/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('Render Product Pepsi', () => {
-  const { getByText } = render(<ProductsList products={products} foodCategories={categories} drinkCategories={categories} ingredients={ingredients} />);
+  const { getByText } = render(<ProductsList products={categories.products} foodCategories={categories} categories={categories} drinkCategories={categories} ingredients={ingredients} />);
   const linkElement = getByText(/Pepsi/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 it('ProductsList renders correctly', () => {
   const tree = renderer
-    .create(<ProductsList products={products} foodCategories={categories} drinkCategories={categories} />)
+    .create(<ProductsList products={categories.products} foodCategories={categories} categories={categories} drinkCategories={categories} />)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
